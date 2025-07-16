f8ab16aae2eb4a05e6609e99fd0791bbbafd7694
RoshanPatilCode <roshanpatil2522000@gmail.com>
2025-07-16 18:51:26 +0530

    The First Default Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
 import "./App.css";
-import MainApp from './Components/MainApp'
+import MainApp from "./Components/MainApp";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-       <MainApp></MainApp>
+      <MainApp></MainApp>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
